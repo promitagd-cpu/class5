b90bab691dcf7a4d5b10d78f35fac1959f08281d
promitagd-cpu <promitagd@gmail.com>
2026-06-27 12:38:09 +0530

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,9 @@
+temperature = 38
+is_raining = False
+ 
+if is_raining:
+    print ("Wear a raincoat, not light clothes.")
+elif temperature >= 25:
+    print ("Perfect weather for light cotton clothes!")
+else :
+    print ("Too cold for light clothes. Wear a jacket.")
